2dc5abb7fe8aae358d1063929ce4acb298630c01
Tharindu <tharindu.damintha@gmail.com>
2016-04-27 11:43:58 +0530

    Initial commit

---
+++ CustomSeekBar/app/src/main/java/com/android/tdsoft/customseekbar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.android.tdsoft.customseekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ((TextView)findViewById(R.id.textView)).setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
